5e18be52a7175d7028bbc1d791c6c3a5ba5a7667
Chao Liu <liuchao8989@gmail.com>
2016-10-21 21:27:44 -0700

    add message ui in index.html and fix code order

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,21 @@
 var socket = io();
 
-socket.on('connect',function(){
+socket.on('connect', function() {
   console.log("yep I connected");
 })
 
-socket.on('message',function(message){
-  console.log('this is message from server: '+ message.text);
-})
+socket.on('message', function(message) {
+  console.log('this is message from server: ' + message.text);
+});
+
+
+var $form = $('#message-form');
+$form.on('submit', function(event) {
+  event.preventDefault();
+  var $message = $form.find('input[name=message]');
+  socket.emit('message', {
+    text: $message.val()
+  });
+  $message.val('');
+
+});
--- server.js
+++ server.js
@@ -8,15 +8,16 @@ app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket) {
   console.log('user connect vai socket io');
+
   socket.on('message', function(message) {
     console.log("message  received: " + message.text);
     //io.emit is for send to all including sender
-
     //send to all except sender
     socket.broadcast.emit('message', message);
   })
+
   socket.emit('message', {
-    text: "server: Welcome this is only for test"
+    text: "server: Welcome"
   });
 });
 http.listen(PORT, function() {
