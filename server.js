eb9cd86c6f0eb43d86b831bbd41adc2c9187d3c1
Chao Liu <liuchao8989@gmail.com>
2016-10-21 19:38:51 -0700

    add socket connection

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
-
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-http.listen(PORT, function(){
+io.on('connection', function() {
+  console.log('user connect vai socket io');
+});
+http.listen(PORT, function() {
   console.log('Server start!');
 })
